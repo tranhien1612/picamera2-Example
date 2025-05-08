cc12e2859a96bf12f0a5c521014702ba4a4c89af
Trần Bá Hiến <69721832+tranhien1612@users.noreply.github.com>
2025-05-08 14:12:39 +0700

    Update testRTSP.py

--- testRTSP.py
+++ testRTSP.py
@@ -4,6 +4,18 @@
 # sudo apt install gstreamer1.0-tools gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav
 # sudo apt install gstreamer1.0-rtsp && sudo apt install python3-gst-1.0 && sudo apt install gir1.2-gst-rtsp-server-1.0
 # sudo apt install python3-gi gir1.2-gst-plugins-base-1.0 gir1.2-gstreamer-1.0
+
+'''
+gst-launch-1.0 videotestsrc is-live=true ! videoconvert ! x264enc bitrate=1000 tune=zerolatency ! video/x-h264 ! h264parse ! video/x-h264 ! queue ! flvmux name=mux ! \
+    rtmpsink location="rtmp://a.rtmp.youtube.com/live2/f86d-994t-kbp3-xzm2-fhjf" audiotestsrc is-live=true ! \
+    audioconvert ! audioresample ! audio/x-raw,rate=48000 ! voaacenc bitrate=96000 ! audio/mpeg ! aacparse ! audio/mpeg, mpegversion=4 ! mux.
+
+
+gst-launch-1.0 rtspsrc location=rtsp://127.0.0.1:8554/test ! decodebin3 ! videoconvert ! x264enc bitrate=4000 tune=zerolatency ! video/x-h264 ! h264parse ! video/x-h264 ! queue ! flvmux name=mux ! \
+    rtmpsink location="rtmp://a.rtmp.youtube.com/live2/f86d-994t-kbp3-xzm2-fhjf" audiotestsrc is-live=true ! \
+    audioconvert ! audioresample ! audio/x-raw,rate=48000 ! voaacenc bitrate=96000 ! audio/mpeg ! aacparse ! audio/mpeg, mpegversion=4 ! mux.
+'''
+
 import gi
 
 gi.require_version('Gst', '1.0')
